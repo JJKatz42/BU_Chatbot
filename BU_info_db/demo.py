6bb5c45f870342a30e56543e68146202c2c9f8c4
JJKatz42 <62679416+JJKatz42@users.noreply.github.com>
2023-08-02 18:22:33 +0000

    d

--- BU_info_db/demo.py
+++ BU_info_db/demo.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 import langchain.chat_models
 
@@ -6,37 +7,40 @@ import langchain.chat_models
 # import libs.config as config
 # import libs.search as search
 # import libs.storage as storage
+import config
 from weaviate_store import WeaviateStore
 from search_agent import SearchAgentFeatures, SearchAgent
 from weaviate_search_engine import WeaviateSearchEngine
 
 
+def init_config(local_env_file: str | None):
+    config.init(
+        metadata=[
+            config.ConfigVarMetadata(var_name="DATA_NAMESPACE"),
+            config.ConfigVarMetadata(var_name="WEAVIATE_URL"),
+            config.ConfigVarMetadata(var_name="WEAVIATE_API_KEY"),
+            config.ConfigVarMetadata(var_name="OPENAI_API_KEY"),
+        ],
+        local_env_file=local_env_file
+    )
 
-# def init_config(local_env_file: str | None):
-#     config.init(
-#         metadata=[
-#             config.ConfigVarMetadata(var_name="DATA_NAMESPACE"),
-#             config.ConfigVarMetadata(var_name="WEAVIATE_URL"),
-#             config.ConfigVarMetadata(var_name="WEAVIATE_API_KEY"),
-#             config.ConfigVarMetadata(var_name="OPENAI_API_KEY"),
-#         ],
-#         local_env_file=local_env_file
-#     )
-
-OPENAI_API_KEY="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"
-DATA_NAMESPACE="Jonahs_weaviate"
-WEAVIATE_URL="https://bu-cluster-2-o5pekqq0.weaviate.network"
-WEAVIATE_API_KEY="vXNsRxv6vSJ57r0JKOJxhlBwMDIBadbyvjGC"
 
 
 async def main():
     # init_config(".env")
+    env_file = ".env"
+    if not env_file.startswith("/"):
+        current_directory = os.path.dirname(__file__)
+        env_file = os.path.join(current_directory, env_file)
+    init_config(local_env_file=env_file)
+
     # Initialize weaviate store
     weaviate_store = WeaviateStore(
-        instance_url=WEAVIATE_URL,
-        api_key=WEAVIATE_API_KEY,
-        openai_api_key=OPENAI_API_KEY,
-        namespace=DATA_NAMESPACE
+        instance_url=config.get("WEAVIATE_URL"),
+        api_key=config.get("WEAVIATE_API_KEY"),
+        openai_api_key=config.get("OPENAI_API_KEY"),
+        namespace=config.get("DATA_NAMESPACE"),
+        cohere_api_key=config.get("COHERE_API_KEY")
     )
     # Initialize a search engine
     weaviate_search_engine = WeaviateSearchEngine(weaviate_store=weaviate_store)
@@ -44,7 +48,7 @@ async def main():
     reasoning_llm = langchain.chat_models.ChatOpenAI(
         model_name="gpt-3.5-turbo-0613",
         temperature=0.0,
-        openai_api_key=OPENAI_API_KEY
+        openai_api_key=config.get("OPENAI_API_KEY")
     )
     # Initialize the agent with the search engine and reasoning llm
     agent = SearchAgent(
@@ -55,14 +59,12 @@ async def main():
             # SearchAgentFeatures.QUERY_PLANNING,
             # SearchAgentFeatures.AUTO_SEARCH_PARAMETER_GEN
         ],
-        # Uncomment below line to only use Slack threads as sources
-        # include_source_types=[storage.data_classes.Thread]
     )
 
     # Run the agent
     query = "what is the description for sm 132"
     result = await agent.run(query=query)
-    print(result)
+    # print(result)
 
 
 if __name__ == '__main__':
